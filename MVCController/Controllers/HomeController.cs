eb960a9d89bfc591c0842082d75a9f12770ecc66
iprasadreddy <irigalaprasadreddy@gmail.com>
2024-04-13 15:00:08 +0530

    Add project files.

---
+++ MVCController/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace MVCController.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public string Greet()
+        //{
+        //    return "Welcome to mvc5";
+        //}
+        public ActionResult Index() 
+        {
+            return View();
+        }
+        public ActionResult Aboutus()
+        {
+            return View();
+        }
+        public ActionResult Contactus()
+        {
+            return View();
+        }
+        public ActionResult Blogs()
+        {
+            return View();
+        }
+    }
+}
