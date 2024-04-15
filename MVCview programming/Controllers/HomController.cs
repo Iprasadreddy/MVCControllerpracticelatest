e5834d5e6043f117f33c826f8fe72c8670b3260f
iprasadreddy <irigalaprasadreddy@gmail.com>
2024-04-15 12:47:59 +0530

    views in ASP.netmvc

---
+++ MVCview programming/Controllers/HomController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace MVCview_programming.Controllers
+{
+    public class HomController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+        public ActionResult Index3()
+        {
+            return View();
+        }
+        public ActionResult Index4()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCview programming/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCview_programming.MvcApplication" Language="C#" %>
